4fc21e1b65102d2c7f1533051422c8462fd6e9e9
tiwaryshreshtha <115244786+tiwaryshreshtha@users.noreply.github.com>
2023-10-05 15:08:17 +0530

    Update and rename PreprocessingStep.java to DataPreprocessing.java

--- DataPreprocessing.java
+++ DataPreprocessing.java
@@ -3,7 +3,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-public class PreprocessingStep {
+public class Preprocessing {
     public static void main(String[] args) {
         String cremaDirectory = "AudioWAV";
         File[] cremaFiles = new File(cremaDirectory).listFiles();
